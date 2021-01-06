e78fccbfb82c5be90e9fb47ef8fcf358e1b9eb61
Hanan Yusuf <afetyusuf.gggnc@gmail.com>
2021-01-05 19:13:13 -0600

    working app but not live on heroku yet

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# workout-tracker
+# workout-tracker 
+
+## Description
+
+This is a workout tracker application that enable users to view, create and track their daily workouts. The user can also log multiple exercises in a workout on a given day and track the name type, wight, sets, reps, and duration of the exercise. Users can also track distance traveled, if the exercise is cardio. 
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [contributing](#credits)
+* [Issues](#issues)
+* [Questions](#questions)
+
+## Installation
+The program uses Mongo database with a Mongoose schema and Express to handle routes.
+
+## Usage
+To run this application locally, Install the dependencies and verify database connection information. Run `node server.js` on your terminal to start the server and follow the link or open it on your browser to view the application on your computer(locally). The application running on Heroku can be found [Here]()
+
+
+![](image/img.png)
+
+## Contribution
+
+When contributing to this repository, please first discuss the change you wish to make via issue, email, or any other method with the owners of this repository before making a change.
+
+## Issues
+
+Feel free to submit issues and enhancement requests.
+
+
+## Questions?
+
+For any questions you may have, feel free to find me via the information provided below:
+
+Email:
+afetyusuf.gggnc@gmail.com
+
+[Github Page](https://github.com/HananYusuf/)
+HananYusuf
+
---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const db = require("./models");
+const path = require("path")
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+//HTML routes
+
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/exercise.html"))
+})
+
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/stats.html"))
+})
+
+//API routes
+
+app.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+        .then(data => {
+            res.json(data)
+        })
+})
+
+app.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({})
+        .then(data => {
+            res.json(data)
+        })
+})
+
+app.post("/api/workouts", (req, res) => {
+    db.Workout.create({})
+        .then(newWorkout => {
+            res.json(newWorkout)
+        })
+})
+
+app.put("/api/workouts/:id", (req, res) => {
+    db.Workout.update({_id: req.params.id},
+        {
+            $push: {exercises: req.body}
+        }).then(dbUpdated => {
+            res.send(dbUpdated);
+        })
+})
+
+app.listen(PORT, () => {
+    console.log(`App is running on ${PORT}`)
+})
+
+module.exports=app;
